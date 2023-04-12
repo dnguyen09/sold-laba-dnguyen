0edb311bd0ef8614b162f8ac323a566b4862bc69
Duy Nguyen <mosarhh@gmail.com>
2023-04-12 11:57:28 -0600

    updated comment

--- src/com/solvd/laba/lab1/UserInfo.java
+++ src/com/solvd/laba/lab1/UserInfo.java
@@ -2,8 +2,10 @@ package com.solvd.laba.lab1;
 
 public class UserInfo {
     public static void main(String[] args) {
-        
+        //assign name
         String name = args[0];
+
+        //print out name
         System.out.println("Name: " + name);
     }
 }
