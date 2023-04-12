7f283580619a869e518f357bb7eb76e59c0c5f23
Duy Nguyen <mosarhh@gmail.com>
2023-04-12 11:44:58 -0600

    First Project commit

---
+++ src/com/solvd/laba/lab1/UserInfo.java
@@ -0,0 +1,9 @@
+package com.solvd.laba.lab1;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        
+        String name = args[0];
+        System.out.println("Name: " + name);
+    }
+}
