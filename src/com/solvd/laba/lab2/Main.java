ec3aadd649bfeba317d66e8cce1791d1ec6d3ced
Duy Nguyen <mosarhh@gmail.com>
2023-04-24 22:34:29 -0600

    change to main

--- src/com/solvd/laba/lab2/Main.java
+++ src/com/solvd/laba/lab2/Main.java
@@ -15,7 +15,6 @@ public class Main {
         System.out.println(result2);
 
         Account account1 = bank.createAccount(cus1, 400);
-
         System.out.println(account1.getAccountNumber());
 
         CheckingAccount check1 = bank.createCheckingAccount(account1);
@@ -26,45 +25,8 @@ public class Main {
         bank.checkMonthlyFee(check1);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        Account acc1 = new Account(1112223,300,"checking account",cus2);
-//        CheckingAccount check1 = new CheckingAccount(acc1, 12.0);
-//
-//        System.out.println("Account number: " + check1.getAccountNumber());
-//        System.out.println("Checking account balance: " + check1.getBalance());
-//
-//        check1.chargeMonthlyFee();
-//        System.out.println("Balance after monthly fee: " + check1.getBalance());
-//        check1.deposit(300);
-//        System.out.println("Balance after deposit $200: " + check1.getBalance());
-//        check1.chargeMonthlyFee();
-//
 //        SavingAccount sav1 = new SavingAccount(acc1, "Saving account",1500,0.15);
 //        System.out.println("Saving account balance: " + sav1.getBalance());
 //        System.out.println("Interest Earned: " + "$" + sav1.getInterestEarned());
-
-
-
-
-
-
-
-
-
-
-
     }
 }
